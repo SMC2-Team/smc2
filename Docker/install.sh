7574410ebc98f3fd13bb7f83f2406f7c82c54790
smccompiler <101070811+smccompiler@users.noreply.github.com>
2022-03-11 01:09:38 -0500

    Update install.sh

--- Docker/install.sh
+++ Docker/install.sh
@@ -1,13 +1,59 @@
-cd smc2
+# clone the picco repository
+git clone https://github.com/PICCO-Team/picco.git
 
-# install 
-cd compiler
+
+# add our benchmarking programs and scripts to the PICCO directories
+cp -r smc2/compute/example-programs picco/compute/
+cp smc2/compute/makefile picco/compute/makefile
+cp smc2/compute/programs.mk picco/compute/programs.mk
+cp smc2/compute/smc-config picco/compute/smc-config
+cp smc2/compute/runtime-config picco/compute/runtime-config
+
+
+# install PICCO
+cd picco/compiler
 mkdir bin
 bash compile.sh
-cp bin/* /usr/bin
+
+cd ../compute
+mkdir bin
+cp ../compiler/bin/* bin/
 
 # generate keys
+mkdir keys
+cd keys
+for ID in {1..3}
+do
+  openssl genrsa -out privkey$ID.pem
+  openssl rsa -in privkey$ID.pem -outform PEM -pubout -out pubkey$ID.pem
+done
+
+cd ../
+# list of programs to compile
+programs=(pay-gap LR-parser h_analysis private-branching private-branching-add private-branching-mult private-branching-reuse)
+
+cd example-programs
+
+# compile, then create input shares
+for prog in ${programs[@]}
+do
+   bash compile.sh "$prog"
+done
+
+cd ../../../
+
+
+
+# install SMC2
+cd smc2/compiler
+mkdir bin
+bash compile.sh
+
 cd /root/smc2/compute
+mkdir bin
+cp ../compiler/bin/* bin/
+
+# generate keys
 mkdir keys
 cd keys
 for ID in {1..3}
@@ -26,24 +72,7 @@ cd example-programs
 # compile, then create input shares
 for prog in ${programs[@]}
 do
-   echo compiling "$prog"
-   echo picco "$prog"/"$prog".c ../smc-config "$prog" "$prog"/utility
-   picco "$prog"/"$prog".c ../smc-config "$prog" "$prog"/utility
-   echo creating input shares with picco-utility
-   echo picco-utility -I 1 "$prog"/input.txt "$prog"/utility "$prog"/input
-   picco-utility -I 1 "$prog"/input.txt "$prog"/utility "$prog"/input
-   mv "$prog".cpp ../"$prog".cpp
+   bash compile.sh "$prog"
 done
 
-cd ../
-# make executables
-make
-
-# move things back into their proper directories
-for prog in ${programs[@]}
-do
-   mv "$prog".cpp example-programs/"$prog"/"$prog".cpp
-   mv "$prog".o example-programs/"$prog"/"$prog".o
-   mv "$prog" example-programs/"$prog"/"$prog"
-done
 
